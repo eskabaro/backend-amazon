2067a058bd1dcc9244f456d58137db2316b9d09b
eskabaro <kononucenkosasa4004@gmail.com>
2023-07-06 09:24:50 +0300

    server updated

--- index.js
+++ index.js
@@ -7,4 +7,19 @@ const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.use((req, res, next) => {
+   res.setHeader("Access-Control-Allow-Origin", "*"); // * разрешает доступ со всех источников, можно указать конкретный источник
+   res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS" // Разрешенные HTTP-методы
+   );
+   res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization" // Разрешенные заголовки
+   );
+   next();
+});
+
+server.listen(port, () => {
+   console.log(`JSON Server is running on port ${port}`);
+});
