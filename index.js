9ab390e78ae320a77a04fad117fbb15cbb046760
eskabaro <kononucenkosasa4004@gmail.com>
2023-07-28 22:09:52 +0300

    deploy server

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
 server.use((req, res, next) => {
-   res.setHeader("Access-Control-Allow-Origin", "*"); // * разрешает доступ со всех источников, можно указать конкретный источник
+   res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+   res.header('Access-Control-Allow-Headers', '*') // * разрешает доступ со всех источников, можно указать конкретный источник
    res.setHeader(
       "Access-Control-Allow-Methods",
       "GET, POST, PUT, DELETE, OPTIONS" // Разрешенные HTTP-методы
@@ -23,3 +24,5 @@ server.use(router);
 server.listen(port, () => {
    console.log(`JSON Server is running on port ${port}`);
 });
+
+// http://localhost:8080
